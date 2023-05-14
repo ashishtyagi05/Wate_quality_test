7d88261d8be81493d416b33b629fefc30b815efe
Ashish Tyagi <100212068+ashishtyagi05@users.noreply.github.com>
2023-05-14 19:38:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,57 @@
+// Import necessary packages and libraries
 const express = require("express");
 const http = require("http");
 const https = require("https");
 const dotenv = require("dotenv");
+
+// Initialize the app and create a server
 const app = express();
 const server = http.createServer(app);
+
+// Load environment variables
 dotenv.config();
+
+// Serve static files from the public directory
 app.use(express.static("public"));
+
+// Listen on the specified port
 server.listen(process.env.PORT, (err) => {
   if (err) {
     throw err;
   } else {
-    console.log(`Server in running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
   }
 });
 
+// Initialize socket.io and listen for connections
 const io = require("socket.io")(server);
 io.on("connection", (socket) => {
-  console.log("connection!");
+  console.log("A user has connected!");
 
-  https.get(process.env.URL, (response) => {
+  // Set the Thingspeak URL to retrieve data from
+  const url = "https://api.thingspeak.com/channels/2120838/feeds.json?results=1";
+
+  // Make an HTTPS request to retrieve the data
+  https.get(url, (response) => {
     response.on("data", (data) => {
+      // Parse the response data into a JavaScript object
       const dataOut = JSON.parse(data);
 
+      // Extract the desired values from the object
       const output = {
         tds: dataOut.feeds[0].field1,
         temperature: dataOut.feeds[0].field2,
         ec: dataOut.feeds[0].field3,
       };
       console.log(output);
+
+      // Send the extracted data to the client via socket.io
       socket.emit("data", output);
     });
   });
 });
 
+// Serve the main HTML page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html", (err) => {
     console.log(err);
